1ed8ec62c88fae5575fe6f88a248500f70bd0dfe
Eric RANDRIANARISOA <raneric@gmail.com>
2023-06-25 15:02:49 +0300

    adding bottom appbar

--- app/src/main/java/com/example/cinematics/ui/MainActivity.kt
+++ app/src/main/java/com/example/cinematics/ui/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.cinematics.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cinematics.R
+import com.example.cinematics.ui.appbar.AppBar
+import com.example.cinematics.ui.components.bottomnav.BottomNavItemVariant
+import com.example.cinematics.ui.components.bottomnav.BottomNavScreen
+import com.example.cinematics.ui.components.bottomnav.bottomNavItemList
 import com.example.cinematics.ui.ui.theme.CinematicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,25 +29,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CinematicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                CinematicsApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CinematicsApp() {
+    Scaffold(
+        topBar = { AppBar() },
+        bottomBar = { BottomNavScreen(bottomNavItemList) }
+    ) { padding ->
+        ContentScreen(modifier = Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background) {
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun CinematicsAppPreview() {
     CinematicsTheme {
-        Greeting("Android")
+        CinematicsApp()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
+         showBackground = true)
+@Composable
+fun CinematicsAppPreviewDark() {
+    CinematicsTheme {
+        CinematicsApp()
     }
 }
---
+++ app/src/main/java/com/example/cinematics/ui/components/bottomnav/BottomNavigation.kt
@@ -0,0 +1,82 @@
+package com.example.cinematics.ui.components.bottomnav
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cinematics.R
+import com.example.cinematics.ui.ui.theme.CinematicsTheme
+
+
+@Composable
+fun BottomNavScreen(bottomNavList: List<BottomNavItemVariant>,
+                    activeTab: BottomNavItemVariant = BottomNavItemVariant.Trending,
+                    modifier: Modifier = Modifier) {
+
+    NavigationBar(tonalElevation = 5.dp) {
+        bottomNavList.forEach { item ->
+            NavigationBarItem(selected = item == activeTab,
+                              icon = {
+                                  Icon(painter = painterResource(id = item.iconId),
+                                       contentDescription = stringResource(id = item.iconContentDescription))
+                              },
+                              label = { Text(text = stringResource(id = item.textId)) },
+                              onClick = { /*TODO*/ })
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavScreenPreview() {
+    CinematicsTheme {
+        BottomNavScreen(bottomNavItemList)
+    }
+
+}
+
+sealed class BottomNavItemVariant(@StringRes val textId: Int,
+                                  @StringRes val iconContentDescription: Int,
+                                  @DrawableRes val iconId: Int,
+                                  val route: String) {
+    object Trending : BottomNavItemVariant(textId = R.string.txt_trending,
+                                           iconContentDescription = R.string.content_descrip_trending,
+                                           iconId = R.drawable.icon_trending_24,
+                                           route = BottomNavTab.TRENDING.route)
+
+    object TopRated : BottomNavItemVariant(textId = R.string.txt_top_rated,
+                                           iconContentDescription = R.string.content_descrip_top_rated,
+                                           iconId = R.drawable.icon_top_rated_24,
+                                           route = BottomNavTab.TOP_RATED.route)
+
+    object WatchList : BottomNavItemVariant(textId = R.string.txt_watch_list,
+                                            iconContentDescription = R.string.content_descrip_watch_list,
+                                            iconId = R.drawable.icon_watch_list_24,
+                                            route = BottomNavTab.WATCH_LIST.route)
+}
+
+enum class BottomNavTab(val route: String) {
+    TRENDING("trending"),
+    TOP_RATED("top-rated"),
+    WATCH_LIST("watch_list")
+}
+
+val bottomNavItemList = listOf(BottomNavItemVariant.Trending,
+                               BottomNavItemVariant.TopRated,
+                               BottomNavItemVariant.WatchList)
