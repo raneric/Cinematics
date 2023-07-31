94fb82a9052e7224c8b25eec1db6f589a0706bca
Eric RANDRIANARISOA <raneric@gmail.com>
2023-07-31 21:54:58 +0300

    adding horizontal list

--- app/src/main/java/com/example/cinematics/ui/content/MovieListScreen.kt
+++ app/src/main/java/com/example/cinematics/ui/content/MovieListScreen.kt
@@ -1,21 +1,26 @@
 package com.example.cinematics.ui.content
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cinematics.data.model.MovieModel
 import com.example.cinematics.data.movieList
+import com.example.cinematics.ui.commonui.BackDrop
 import com.example.cinematics.ui.components.MovieCad
-import kotlinx.coroutines.flow.Flow
+import com.example.cinematics.ui.components.VerticalMovieCard
 
 @Composable
-fun MovieListScreen(movieList: List<MovieModel>,
-                    modifier: Modifier = Modifier,
-                    onItemClicked: (Int) -> Unit) {
+fun VerticalMovieListScreen(movieList: List<MovieModel>,
+                            modifier: Modifier = Modifier,
+                            onItemClicked: (Int) -> Unit) {
 
     LazyColumn(modifier = modifier) {
         items(movieList) {
@@ -26,10 +31,37 @@ fun MovieListScreen(movieList: List<MovieModel>,
     }
 }
 
+
+@Composable
+fun HorizontalMovieListScreen(movieList: List<MovieModel>,
+                              modifier: Modifier = Modifier,
+                              onItemClicked: (Int) -> Unit) {
+
+    Box(contentAlignment = Alignment.Center) {
+        BackDrop(imageId = movieList[0].picture)
+        LazyRow(modifier = modifier,
+                horizontalArrangement = Arrangement.spacedBy(32.dp)) {
+            items(movieList) {
+                VerticalMovieCard(movie = it, modifier = Modifier.clickable {
+                    onItemClicked(it.id)
+                })
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 fun MovieListScreenPreview() {
-    MovieListScreen(movieList) {
+    VerticalMovieListScreen(movieList) {
+
+    }
+}
+
+@Preview
+@Composable
+fun HorizontalMovieListScreenPreview() {
+    HorizontalMovieListScreen(movieList) {
 
     }
 }
