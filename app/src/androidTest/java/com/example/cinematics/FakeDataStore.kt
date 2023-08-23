b7c1f2ab3eb1f5bde50f1faded675bc466b3d949
Eric RANDRIANARISOA <raneric@gmail.com>
2023-08-23 13:33:03 +0300

    fix: change deprecated object in FakeDataStore

--- app/src/androidTest/java/com/example/cinematics/FakeDataStore.kt
+++ app/src/androidTest/java/com/example/cinematics/FakeDataStore.kt
@@ -5,12 +5,12 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.test.core.app.ApplicationProvider
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
 
 private const val TEST_DATA_STORE = "test_pref_datastore"
-private val testDispacher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-private val coroutineScope: TestCoroutineScope = TestCoroutineScope(testDispacher + Job())
+private val testDispatcher = StandardTestDispatcher()
+private val coroutineScope = TestScope(testDispatcher + Job())
 private val testContext: Context = ApplicationProvider.getApplicationContext()
 
 val testDataStore = PreferenceDataStoreFactory.create(
